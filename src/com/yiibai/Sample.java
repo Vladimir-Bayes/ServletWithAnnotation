e76cbe61d4a84c0fba07114cfbae8b0571e10e0d
Vladimir-Bayes <gyang.shines@gmail.com>
2017-12-26 17:20:39 +0800

    Initial commit

---
+++ src/com/yiibai/Sample.java
@@ -0,0 +1,35 @@
+package com.yiibai;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Sample
+ */
+@WebServlet("/Simple")
+public class Sample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+
+		out.print("<html><body>");
+		out.print("<div style='text-align:center;'><h3>Hello Servlet</h3>");
+		out.print("<p>这是使用Servlet注解实现模式URL映射的配置示例</p></div>");
+		out.print("</body></html>");
+	}
+
+}
